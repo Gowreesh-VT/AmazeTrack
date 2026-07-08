9b9f942b738ef978d09dd6bdfafd07b7f2be435b
Gowreesh V T <135145326+Gowreesh-VT@users.noreply.github.com>
2026-07-08 18:15:22 +0530

    pwa: setup service worker, web manifest configuration and assets for offline support

---
+++ src/app/sw.ts
@@ -0,0 +1,21 @@
+import { defaultCache } from "@serwist/next/worker";
+import type { SerwistGlobalConfig } from "serwist";
+import { Serwist } from "serwist";
+
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    __SW_MANIFEST: (string | { revision: string; url: string })[];
+  }
+}
+
+declare const self: WorkerGlobalScope & typeof globalThis;
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: defaultCache,
+});
+
+serwist.addEventListeners();
